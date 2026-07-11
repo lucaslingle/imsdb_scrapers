abc775ed2a89123c7bd4ba3ad9bb21a998224c25
lucaslingle <lucasdaxlingle@gmail.com>
2026-07-11 13:50:30 -0700

    listing spider

---
+++ imsdb_scrapers/spiders/imsdb_listing_spider.py
@@ -0,0 +1,38 @@
+# example:
+# ```
+#     scrapy crawl imsdb_listing -o all_movies_listing.jsonl
+# ```
+import scrapy
+
+class IMSDBListingSpider(scrapy.Spider):
+    name = "imsdb_listing"
+
+    async def start(self):
+        target_urls = [
+            "https://imsdb.com/genre/Action",
+            "https://imsdb.com/genre/Adventure",
+            "https://imsdb.com/genre/Animation",
+            "https://imsdb.com/genre/Comedy",
+            "https://imsdb.com/genre/Crime",
+            "https://imsdb.com/genre/Drama",
+            "https://imsdb.com/genre/Family",
+            "https://imsdb.com/genre/Fantasy",
+            "https://imsdb.com/genre/Film-Noir",
+            "https://imsdb.com/genre/Horror",
+            "https://imsdb.com/genre/Musical",
+            "https://imsdb.com/genre/Mystery",
+            "https://imsdb.com/genre/Romance",
+            "https://imsdb.com/genre/Sci-Fi",
+            "https://imsdb.com/genre/Short",
+            "https://imsdb.com/genre/Thriller",
+            "https://imsdb.com/genre/War",
+            "https://imsdb.com/genre/Western",
+        ]
+        for url in target_urls:
+            yield scrapy.Request(url, callback=self.parse_genrepage)
+    
+    def parse_genrepage(self, response):
+        for href in response.css("p a::attr(href)").getall():
+            yield {
+                "movie_url": response.urljoin(href)
+            }
